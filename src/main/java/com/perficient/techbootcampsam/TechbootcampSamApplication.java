466aa4e6e4c93cf11e756421437062f57cd4ccf0
Sam Jeon <samueljeon99@gmail.com>
2022-01-17 17:23:48 -0500

    project setup

---
+++ src/main/java/com/perficient/techbootcampsam/TechbootcampSamApplication.java
@@ -0,0 +1,14 @@
+package com.perficient.techbootcampsam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechbootcampSamApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		SpringApplication.run(TechbootcampSamApplication.class, args);
+	}
+
+}
